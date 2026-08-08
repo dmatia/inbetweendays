957de35956bc2844aeb94c7077e09f29edd34959
Diego Villalba <diegovillalba.96@gmail.com>
2026-08-07 21:17:43 -0300

    Poemas

--- poems.js
+++ poems.js
@@ -1,5 +1,4 @@
-const poemasas = [
-    {
+const poemasas = [{
 year: "2025",
 title: "",
 text: `
@@ -24,21 +23,6 @@ la mochila constantemente al lado
 y adonde rumbeo este peso que soy cargo
 porque nunca nadie viene a mí
 siempre soy el que va a todos lados`
-},
-{
-year: "2016",
-title: "",
-text: `
-tal vez sea momento de mirarte de frente y ver como te vas
-caminando en sentido contrario  de esto que soy y esto que no puedo dar
-otra vez acá, parado en este sentimiento
-quieto, incapaz de vos, incapaz de mí
-si solo pudiera decirte lo que quiero
-te diría que te quiero
-si pudiera sacar lo que tengo adentro y dártelo, aunque creo que ya lo hice
-pero no lo agarraste
-solo otra vez. huérfano de vos,
-huérfano de mí, huérfano del don.`
 },
 {
 year: "2019",
@@ -108,43 +92,57 @@ para los que no sabían,
 esta es una historia de amor`
 },
 {
+year: "2025",
+title: "",
+text: `
+el vacío me persigue
+como marca registrada, usa tu perfume
+
+desayuno con pena,
+espero el colectivo con pena
+camino por la calle de los desconsolados
+donde todos mis anhelos
+aprendieron a no ser expresados
+
+es cierto que la viejas costumbres
+solo saben morir gritando
+
+a la sombra de tus emociones
+siempre estoy a punto de perder`
+},
+{
 year: "2021",
 title: "",
 text: `
 quisiera encontrarle la vuelta a algunas cosas. entender cómo funciona el agua, fundirme en la melodía. descubrir qué separa la noche del día y cómo cambio cuando la luna se esconde en la orilla. quisiera desplegarme entre el todo que es esto y entender cómo se entiende la vida y todos esos secretos que se esconden entre los días. quisiera ser dueño de al menos una fantasía. la mano que gira la llave, la rueda que da pie a esa manía.`
 },
 {
-year: "2025",
+year: "2021",
 title: "",
-text: `
-la rueda corta la distancia entre vos y yo
+text: `la rueda corta la distancia entre vos y yo
 decimelo y repetí tus palabras
 que el escuchar con entender no alcanza
 cuando llega el día quiero más
-en la otra partida, quiero más
-y cuando todos se callen
+en al otra partida, quiero más
+y cuando todos se callan
 yo te voy a ir a buscar`
 },
 {
 year: "2023",
 title: "",
-text: `
-no sé cómo comportarme,
+text: `no sé cómo comportarme,
 no sé cómo se hacen las cosas
-en las escenas de nuestra historia rota
-cada cosa se va poniendo cada vez peor
-
-(es muy raro no tenerte cerca)
-
+las escenas de nuestra historia rota
+cada cosa se va poniendo
+cada vez peor
+es muy raro no tenerte cerca
 los días de limpieza
 decíamos que nos estábamos preparando
 pero no sabíamos para qué
-
 no estés enojada conmigo
-no fue para causarte un mal a vos
-todavía no me doy cuenta que algunas cosas ya fueron
-
-¿te acordás de nuestra canción favorita?
+no fue para buscarte un mal a vos
+no me doy cuenta que las cosas ya fueron
+te acordás de nuestra canción favorita?
 ¿cómo se llamaba?`
 },
 {
@@ -159,20 +157,19 @@ y de filosofía
 del café, de los perros
 y la vecina de la esquina
 repitamos esa charla que ya tuvimos
-agreguemos algo más,
+agreguemos algo más
 algo que no dijimos
-si hace falta mentir, mintamos
-si hace falta inventar, inventemos
-
+si hace falta mentir, mentimos
+si hace falta inventar, lo hacemos
 busca en la ceniza del cigarro
-detrás de los muebles, debajo de los tarros,
+detrás de los muebles, abajo de los tarros
 apurate a encontrar noticias
 antes de que termine el dia
 repasemos obviedades, que siempre quede algún resto
 no nos encontremos en los ojos
 como dos perdidos
 que no nos gane el silencio
-y nos veamos obligados
+y no nos veamos obligados
 sin más remedio
 a tener que hablar de todo esto`
 },
@@ -186,9 +183,7 @@ los días de verano
 la cotidianidad del siempre
 los fuera de foco
 esperas y adioses
-
 y vuelven
-
 mis dias vuelven a ser míos
 y tuyos vuelven a ser tus días
 y la espera, otros labios, y la espera
@@ -196,13 +191,11 @@ y se desvanecen los días
 entrada ya la noche
 mientras me coso
 y trato de arreglarme
-
 hasta que tus días 
 se entrecrucen con mis días
 y todo es verde
 pero la soledad ya no se siente acompañada
 y todo es celeste
-
 y vuelven
 la nada
 los días de verano
@@ -211,39 +204,37 @@ la cotidianidad del siempre`
 {
 year: "2016",
 title: "",
-text: `
-tal vez sea momento de mirarte de frente y ver cómo te vas
-caminando en sentido contrario  de esto que soy y de esto que no puedo dar
+text: `tal vez sea momento de mirarte de frente y ver como te vas
+caminando en sentido contrario  de esto que soy y esto que no puedo dar
 otra vez acá, parado en este sentimiento
 quieto, incapaz de vos, incapaz de mí
-si tan solo pudiera decirte lo que quiero, te diría que te quiero
+si tan solo pudiera decirte lo que quiero
+te diría que te quiero
 si pudiera sacar lo que tengo adentro y dártelo, no lo dudaría
 aunque creo que ya lo hice
 pero no lo agarraste y desde la partida,
-solo otra vez, huérfano de mí,
-huérfano de vos, huérfano del don.`
+solo otra vez, huérfano de vos,
+huérfano de mí, huérfano del don.`
 },
 {
 year: "2025",
 title: "",
-text: `
-las plantas crecen y vuelven a florecer
+text: `las plantas crecen y vuelven a florecer
 crece el río por las piedras que quiero aprender
 no importa si el reloj camina callado
 siempre voy a encontrar en algo volver
-aclara la noche cuando te encuentro en La Boca
+aclara la noche cuando te encuentro en la boca
 bailando en la parte de atrás de mi cabeza
 sabés que me estoy haciendo de todo esto
 hay ideas que no quiero entender
-pero por vos me muevo un poco, y otro más
+por vos me muevo un poco, y otro más
 y si algo falla y no me encontrás
 inventando cielos que nos cobijen voy a estar`
 },
 {
 year: "2025",
 title: "",
-text: `
-sos la ausencia de mis padres
+text: `sos la ausencia de mis padres
 del cuidado que no supe tener
 de la falta de gobierno y duración, sos la acción 
 de todas las veces que tropecé
@@ -255,9 +246,63 @@ un secreto
 una daga
 sos como el verano que se va y yo soy yo
 como soy cuando me quedo`
-}
+},
+{
+year: "2019",
+title: "",
+text: `
+me alejo ciertamente de las cosas 
+parado en la orilla del todo 
+miro cómo camina el reloj 
+y me voy de la idea de mí 
+y al hacerlo me voy también de la idea de vos 
+
+completa la tarde una brisa fresca del fin de septiembre 
+el invierno se resiste a la muerte con un eco 
+y yo lo entiendo 
+como empiezo a entender que tal vez sea hora de juntar mis cosas 
+de guardar la tinta y el tintero 
+de dejar morir el último intento 
+de salir para siempre por la puerta que me vio entrar 
+antes de todo esto`
+},
+{
+year: "2022",
+title: "",
+text: `
+un sabor o un aroma que me lleva a una golosina de mi infancia
+ahí te encuentro
+lo que escribí en el margen de la hoja aquel día, en aquel diario (¿será tu nombre?)
+en el silencio del domingo a la tarde cuando todo termina y vuelve a empezar	(ahí en el medio)
+en el jazmín que se prende de la pared equivocada - no puedo juzgar
+en el sol que dejó una huella en la ventana
+en la fragilidad del intento
+en la multitud del subte
+en el frío de la plaza
+en esta ciudad llena de vacío`
+},
+{
+year: "2022",
+title: "Umbrales",
+text: `
+en el fondo de la mañana 
+en el borde de la orilla 
+una recorrida solitaria 
+todo cubierto de blanco 
+tentado en una manía 
+cuando se apaga la hora del sol buscando en el borde de la tarde 
+el puerto duerme solitario 
+unos pocos adentro se refugian de la noche 
+desde el bosquejo de un día agitado 
+los navíos a lo lejos desaparecen 
+la forma de la mañana asecha del otro lado 
+miramos lo mismo pero vemos diferente 
+
+¿qué hay después del último después?}
+
 
 ];
+
 
 const poemasContainer = document.querySelector(".poemas");
 const next = document.querySelector(".next");
